a694a32a6482266b95d84063d760b7e1bbcf60ce
ksol46 <doomee880406@naver.com>
2023-03-24 14:19:31 +0900

    d

--- src/main/java/com/myshop/MyshopApplication.java
+++ src/main/java/com/myshop/MyshopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MyshopApplication { ////
+public class MyshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyshopApplication.class, args);
